3a7649dc2badbe05da11706b9c7b9bdd6c307fe8
Kadota <joaokadota@gmail.com>
2022-06-25 17:19:41 -0300

    Sintaxe

--- Sintaxe-e-operadores.js
+++ Sintaxe-e-operadores.js
@@ -6,6 +6,7 @@ function comparaNumeros(num1, num2){
 }
 
 function criaPrimeiraFrase(num1, num2) {
+    let primeiraFrase = `Os números ${num1} e ${num2}`;
     let saoIguais = '';
 
     if(num1 !== num2) {
@@ -18,7 +19,7 @@ function criaPrimeiraFrase(num1, num2) {
 function criaSegundaFrase(num1, num2){
     const soma = num1 + num2;
 
-    let resultado10 = 'maior';
+    let resultado10 = 'menor';
     let resultado20 = 'menor';
     const compara10 = soma > 10;
     const compara20 = soma > 20;
@@ -28,10 +29,10 @@ function criaSegundaFrase(num1, num2){
     }
 
     if(compara20){
-        resultado20 = 'menor';
+        resultado20 = 'maior';
     }
 
     return `Sua soma é ${soma},  que é ${resultado10} que 10 e ${resultado20} que 20`;
 }
 
-console.log(comparaNumeros(1,2));
+console.log(comparaNumeros(10,10));
