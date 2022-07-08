fd80b28323bdc244b8c73b0307ebeb227a3a5a6a
Kadota <joaokadota@gmail.com>
2022-07-08 09:09:13 -0300

    Estudo Java

---
+++ Módulo/main.mjs
@@ -0,0 +1,5 @@
+import {mostraCidade, mostraHobby, mostraIdade} from "./funcoes.mjs";
+
+console.log(mostraIdade('João', 33));
+console.log(mostraCidade('João', 'São Paulo'));
+console.log(mostraHobby('João', 'Judô'));
